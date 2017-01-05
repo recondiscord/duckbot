4ef39868275e0cd85c125edaa8e1fd536df91b97
cduckk <aronmoberg@Aron-Mobergs-iMac.local>
2017-01-05 01:28:33 +0100

    changed pullanddeploy
    Updated pullanddeploy to older version;

--- discord_bot.js
+++ discord_bot.js
@@ -526,33 +526,9 @@ var commands = {
             msg.channel.sendMessage("fetching updates...").then(function(sentMsg){
                 console.log("updating...");
 	            var spawn = require('child_process').spawn;
-                var log = function(err,stdout,stderr){
-                    if(stdout){console.log(stdout);}
-                    if(stderr){console.log(stderr);}
-                };
-                var fetch = spawn('git', ['fetch']);
-                fetch.stdout.on('data',function(data){
-                    console.log(data.toString());
-                });
-                fetch.on("close",function(code){
-                    var reset = spawn('git', ['reset','--hard','origin/master']);
-                    reset.stdout.on('data',function(data){
-                        console.log(data.toString());
-                    });
-                    reset.on("close",function(code){
-                        var npm = spawn('npm', ['install']);
-                        npm.stdout.on('data',function(data){
-                            console.log(data.toString());
-                        });
-                        npm.on("close",function(code){
-                            console.log("goodbye");
-                            sentMsg.edit("brb!").then(function(){
-                                bot.destroy().then(function(){
-                                    process.exit();
-                                });
-                            });
-                        });
-                    });
+                spawn('sh', [ 'pullanddeploy.sh' ]).on("close",function(code){
+                    console.log("exiting");
+                    process.exit();
                 });
             });
         }
